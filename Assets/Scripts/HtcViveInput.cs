976301d41f4a7ee8b4c833b2d0720bebc06a43b1
Marina <mbakanova2016@yandex.ru>
2023-02-23 21:07:47 +0300

    Move almost ready

--- Assets/Scripts/HtcViveInput.cs
+++ Assets/Scripts/HtcViveInput.cs
@@ -1,41 +1,43 @@
-using System;
 using UnityEngine;
-using UnityEngine.Serialization;
 using Valve.VR;
-using Valve.VR.InteractionSystem;
 
 public class HtcViveInput : MonoBehaviour
 {
     private SteamVR_Input_Sources _hand;
-    [FormerlySerializedAs("TouchPos")] [SerializeField]
+    [SerializeField] 
     private SteamVR_Action_Vector2 _touchPos;
+    [SerializeField] 
+    private SteamVR_Action_Boolean _touchPad;
 
-    private Vector2 _vectorFromHand =  new Vector2(0, 0);
+    private Vector2 _vectorFromHand = new Vector2(0, 0);
     private CharacterController _characterController;
-    [SerializeField]
+    [SerializeField] 
     private float _speed = 12f;
-    
+
     private void Start()
     {
         _characterController = GetComponent<CharacterController>();
     }
-
-    // ReSharper disable Unity.PerformanceAnalysis
+    
     private Vector2 GetTrackPadPos()
     {
-        Debug.Log(_touchPos.GetAxis(_hand));
         _vectorFromHand = _touchPos.GetAxis(_hand);
         return _touchPos.GetAxis(_hand);
     }
 
+    private bool GetTouchPad()
+    {
+        return _touchPad.GetState(_hand);
+    }
+
     public void Update()
     {
-        //Vector3 vectorFromHandV3 = new Vector3(vectorFromHand.x, 0, vectorFromHand.y);
+        if (!GetTouchPad()) return;
         GetTrackPadPos();
-        if ((!(_vectorFromHand.y > 0.7f)) && (!(_vectorFromHand.y < -0.7f)) && (!(_vectorFromHand.x > 0.7f)) &&
-            (!(_vectorFromHand.x < -0.7f))) return;
-        _characterController.Move(_vectorFromHand * (_speed * Time.deltaTime));
+        if (((!(_vectorFromHand.y > 0.7f)) && (!(_vectorFromHand.y < -0.7f)) && (!(_vectorFromHand.x > 0.7f)) &&
+             (!(_vectorFromHand.x < -0.7f)))) return;
+        var vectorFromHandV3 = new Vector3(-_vectorFromHand.x, 0, -_vectorFromHand.y);
+        _characterController.Move(vectorFromHandV3 * (_speed * Time.deltaTime));
         print("Moving");
-        //getTrackPadPos();
     }
 }
